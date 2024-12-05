c2bd53ee8ef1211649df02f754f237b4926ba3a5
Rania Shafira Ekasanti <2222212836@stis.ac.id>
2024-12-05 16:20:16 +0700

    landingpage

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -35,6 +35,57 @@
         .service:hover {
             transform: scale(1.05);
         }
+
+        .welcome {
+            font-size: 2.5vw;
+            /* Ukuran berdasarkan lebar viewport */
+            color: white;
+            text-align: center;
+            font-family: Georgia, 'Times New Roman', Times, serif;
+            font-weight: bold;
+            padding: 10px;
+        }
+
+        /* Gaya dasar untuk h2 */
+        .welcome2 {
+            font-size: 1.5vw;
+            color: white;
+            text-align: center;
+            font-family: Georgia, 'Times New Roman', Times, serif;
+            padding: 10px;
+        }
+
+        .fitur {
+            min-height: 20rem;
+            /* Set minimum height */
+        }
+
+        /* Responsif dengan media query */
+        @media (max-width: 768px) {
+            .welcome {
+                font-size: 3.5vw;
+                /* Teks lebih besar untuk layar kecil */
+            }
+
+            .welcome2 {
+                font-size: 2.5vw;
+            }
+        }
+
+        @media (max-width: 480px) {
+            .welcome {
+                font-size: 3vw;
+                /* Teks lebih besar untuk layar yang lebih kecil */
+            }
+
+            .welcome2 {
+                font-size: 2vw;
+            }
+        }
+
+        section {
+            border-radius: 40px;
+        }
     </style>
 </head>
 
@@ -42,71 +93,71 @@
     <main class="container mx-auto mt-0" style="background-image: url('<?= base_url('assets/images/bg-welcome.png') ?>'); background-size: cover; background-position: top; height: 50vh;">
         <!-- Video Thumbnail -->
         <div class="text-center mb-8">
+            <h1 class="welcome">SELAMAT DATANG DI PST MENJAWAB</h1>
+            <h2 class="welcome2">PELAYANAN STATUSTIK TERPADU</h2>
+            <h2 class="welcome2">BPS PROVINSI DKI JAKARTA</h2>
             <img src="<?= base_url('assets/images/vid.jpg') ?>" alt="Panduan" class="mx-auto">
             <p class="text-gray-600 mt-2">Panduan User Video</p>
         </div>
 
         <!-- Konsultasi Statistik -->
-        <section class="text-center bg-yellow-500 text-white py-4 mb-8">
+        <section class="text-center bg-biru-4 text-oranye-2 py-2 mb-8 w-1/2 mx-auto">
             <h2 class="text-2xl font-bold">Konsultasi Statistik</h2>
         </section>
 
-        <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
+        <div class="fitur grid grid-cols-1 md:grid-cols-3 gap-8 mx-auto place-items-center">
             <!-- Service 1 -->
-            <div class="bg-white shadow-lg p-6 rounded-lg transform hover:scale-105 transition">
-                <img src="konsultasi-langsung.png" alt="Konsultasi Langsung" class="mx-auto w-12 mb-4">
+            <div class="fitur text-center bg-oranye-3 shadow-lg p-6 rounded-lg transform hover:scale-105 transition max-w-sm">
+                <img src="<?= base_url('assets/images/konsultasi-langsung.png') ?>" alt="Konsultasi Langsung" class="mx-auto w-12 mb-4">
                 <h3 class="text-xl font-semibold">Konsultasi Langsung</h3>
                 <p class="text-gray-600">Deskripsi layanan konsultasi langsung.</p>
             </div>
             <!-- Service 2 -->
-            <a href="/chatbot" class="block bg-white shadow-lg p-6 rounded-lg transform hover:scale-105 transition">
-                <img src="icon2.png" alt="Tanya-Jawab via Chat Bot" class="mx-auto w-12 mb-4">
+            <a href="/chatbot" class="fitur text-center block bg-oranye-3 shadow-lg p-6 rounded-lg transform hover:scale-105 transition max-w-sm">
+                <img src="<?= base_url('assets/images/chatbot.png') ?>" alt="Tanya-Jawab via Chat Bot" class="mx-auto w-12 mb-4">
                 <h3 class="text-xl font-semibold">Tanya-Jawab via Chat Bot</h3>
                 <p class="text-gray-600">Deskripsi layanan tanya-jawab via chat bot.</p>
             </a>
             <!-- Service 3 -->
-            <a href="/consultation" class="block bg-white shadow-lg p-6 rounded-lg transform hover:scale-105 transition">
-                <img src="icon3.png" alt="Konsultasi via Pertemuan Daring" class="mx-auto w-12 mb-4">
+            <a href="/consultation" class="fitur text-center block bg-oranye-3 shadow-lg p-6 rounded-lg transform hover:scale-105 transition max-w-sm">
+                <img src="<?= base_url('assets/images/meeting.png') ?>" alt="Konsultasi via Pertemuan Daring" class="mx-auto w-12 mb-4">
                 <h3 class="text-xl font-semibold">Konsultasi via Pertemuan Daring</h3>
                 <p class="text-gray-600">Deskripsi layanan konsultasi via pertemuan daring.</p>
             </a>
         </div>
 
         <!-- Layanan Lainnya -->
-        <section class="text-center bg-yellow-500 text-white py-4 mt-16">
+        <section class="text-center bg-biru-4 text-oranye-2 py-2 mt-16 w-1/2 mx-auto">
             <h2 class="text-2xl font-bold">Layanan Lainnya</h2>
         </section>
 
         <div class="grid grid-cols-2 md:grid-cols-4 gap-8 mt-8">
             <!-- Service 4 -->
-            <div class="bg-white shadow-lg p-6 rounded-lg transform hover:scale-105 transition">
-                <img src="icon4.png" alt="Pengajuan Permintaan Data" class="mx-auto w-12 mb-4">
+            <div class="fitur text-center bg-oranye-3 shadow-lg p-6 rounded-lg transform hover:scale-105 transition">
+                <img src="<?= base_url('assets/images/cart.png') ?>" alt="Pengajuan Permintaan Data" class="mx-auto w-12 mb-4">
                 <h3 class="text-lg font-semibold">Pengajuan Permintaan Data</h3>
                 <p class="text-gray-600">Deskripsi layanan pengajuan permintaan data.</p>
             </div>
             <!-- Service 5 -->
-            <div class="bg-white shadow-lg p-6 rounded-lg transform hover:scale-105 transition">
-                <img src="icon5.png" alt="Publikasi" class="mx-auto w-12 mb-4">
+            <div class="fitur text-center bg-oranye-3 shadow-lg p-6 rounded-lg transform hover:scale-105 transition">
+                <img src="<?= base_url('assets/images/buku.png') ?>" alt="Publikasi" class="mx-auto w-12 mb-4">
                 <h3 class="text-lg font-semibold">Publikasi</h3>
                 <p class="text-gray-600">Deskripsi layanan publikasi.</p>
             </div>
             <!-- Service 6 -->
-            <div class="bg-white shadow-lg p-6 rounded-lg transform hover:scale-105 transition">
-                <img src="icon6.png" alt="Survei" class="mx-auto w-12 mb-4">
+            <div class="fitur text-center bg-oranye-3 shadow-lg p-6 rounded-lg transform hover:scale-105 transition">
+                <img src="<?= base_url('assets/images/SILATIPA.png') ?>" alt="Survei" class="mx-auto w-12 mb-4">
                 <h3 class="text-lg font-semibold">Survei</h3>
                 <p class="text-gray-600">Deskripsi layanan survei.</p>
             </div>
             <!-- Service 7 -->
-            <div class="bg-white shadow-lg p-6 rounded-lg transform hover:scale-105 transition">
-                <img src="icon7.png" alt="Informasi Terkini" class="mx-auto w-12 mb-4">
+            <div class="fitur text-center bg-oranye-3 shadow-lg p-6 rounded-lg transform hover:scale-105 transition">
+                <img src="<?= base_url('assets/images/papan.png') ?>" alt="Informasi Terkini" class="mx-auto w-12 mb-4">
                 <h3 class="text-lg font-semibold">Informasi Terkini</h3>
                 <p class="text-gray-600">Deskripsi layanan informasi terkini.</p>
             </div>
         </div>
     </main>
-    <div class="container-fluid bg-warning text-white text-center py-3">
-        <p>&copy; 2024 BPS Provinsi DKI Jakarta</p>
-    </div>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
