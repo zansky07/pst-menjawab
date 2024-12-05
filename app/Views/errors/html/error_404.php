220965cf41dff7fc3daa493a694f16fb9e922869
Muhammad Rifky Hidayatullah <222212767@stis.ac.id>
2024-12-05 17:13:13 +0700

    erorr massage

--- app/Views/errors/html/error_404.php
+++ app/Views/errors/html/error_404.php
@@ -4,15 +4,17 @@
     <meta charset="utf-8">
     <title><?= lang('Errors.pageNotFound') ?></title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="<?= base_url('assets/css/styles.css') ?>" rel="stylesheet">
 </head>
 <body class="bg-gray-100">
     <main class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
     <div class="text-center">
-        <p class="text-base font-semibold text-red-600">404</p>
+        <p class="text-base font-semibold text-orange-600">404</p>
         <h1 class="mt-4 text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">Page not found</h1>
         <p class="mt-6 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">Sorry, we couldn’t find the page you’re looking for.</p>
         <div class="mt-10 flex items-center justify-center gap-x-6">
-        <a href="/" class="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">Go back home</a>
+        <a href="/" class="rounded-md bg-orange-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600">Go back home</a>
         <a href="/chatbot" class="text-sm font-semibold text-gray-900">Chatbot <span aria-hidden="true">&rarr;</span></a>
         </div>
     </div>
