04491cb4ffa8b67bd110f14446bd6740d88ad8c8
Esra Yosefa Simarmata <222212585@stis.ac.id>
2024-12-11 08:06:05 +0700

    buat navbar p2

--- app/Views/notifikasi_konsultasi.php
+++ app/Views/notifikasi_konsultasi.php
@@ -227,42 +227,25 @@
         </div>
     </div>
 
-<<<<<<< HEAD
     <div class="relative" id="footer">
 			<img src="/assets/images/footer.png" alt="footer" class="w-full">
 			<div class="absolute inset-0 flex flex-col items-center justify-end text-white text-center px-5 text-lg pb-12">
 				<div class="flex justify-between items-center w-full max-w-6xl mb-8 space-x-8">> <div class="w-1/3 text-left">
-=======
-    <footer class="bg-oranye-1 mt-10">
-    <div class="relative">
-			<img src="/assets/images/footer.png" alt="footer" class="w-full">
-			<div class="absolute inset-0 flex flex-col items-center justify-end text-white text-center px-5 text-lg pb-12">
-				<div class="flex justify-between items-center w-full max-w-6xl mb-8 space-x-8">>
-					<div class="w-1/3 text-left">
->>>>>>> 142c80acfcf5d16721a194ce955876762b77e2bd
 						<div class="flex items-center space-x-4">
 							<img src="/assets/images/logo-pst.png" alt="Logo" class="h-12 w-12">
 							<h3 class="text-xl font-semibold">Badan Pusat Statistik Provinsi DKI Jakarta</h3>
 						</div>
-<<<<<<< HEAD
 						<p class="mt-4 text-base">Jl. Salemba Tengah No. 36-38 Paseban Senen Jakarta Pusat <br>
 							<span>Phone (021) 31928493</span>
 							<br>
 							<span>Fax. (021) 3152004</span>
 							<br>
-=======
-						<p class="mt-4 text-base">
-							Jl. Salemba Tengah No. 36-38 Paseban Senen Jakarta Pusat<br>
-							<span>Phone (021) 31928493</span><br>
-							<span>Fax. (021) 3152004</span><br>
->>>>>>> 142c80acfcf5d16721a194ce955876762b77e2bd
 							<span>E-mail: bps3100@bps.go.id</span>
 						</p>
 					</div>
 					<div class="w-1/3 text-left">
 						<h4 class="text-xl font-semibold">Website Lainnya:</h4>
 						<ul class="list-none text-base">
-<<<<<<< HEAD
 							<li>
 								<a href="https://www.bps.go.id" class="underline">Website BPS Indonesia</a>
 							</li>
@@ -275,18 +258,11 @@
 							<li>
 								<a href="https://silastik.bps.go.id" class="underline">Website SILASTIK</a>
 							</li>
-=======
-							<li><a href="https://www.bps.go.id" class="underline">Website BPS Indonesia</a></li>
-							<li><a href="https://jakarta.bps.go.id" class="underline">Website BPS Provinsi DKI Jakarta</a></li>
-							<li><a href="https://pst.bps.go.id" class="underline">Website Pelayanan Statistik Terpadu</a></li>
-							<li><a href="https://silastik.bps.go.id" class="underline">Website SILASTIK</a></li>
->>>>>>> 142c80acfcf5d16721a194ce955876762b77e2bd
 						</ul>
 					</div>
 					<div class="w-1/3 text-left">
 						<h4 class="text-xl font-semibold">Sosial Media:</h4>
 						<ul class="list-none text-base">
-<<<<<<< HEAD
 							<li>
 								<a href="https://www.facebook.com/bpsdkijakarta/" class="underline">Facebook</a>
 							</li>
@@ -305,21 +281,6 @@
 				<div class="mt-6 text-sm"> &copy; 2024 Badan Pusat Statistik Provinsi DKI Jakarta. All rights reserved. </div>
 			</div>
 		</div>
-=======
-							<li><a href="https://www.facebook.com/bpsdkijakarta/" class="underline">Facebook</a></li>
-							<li><a href="https://x.com/bpsdkijakarta/" class="underline">Twitter</a></li>
-							<li><a href="https://www.instagram.com/bpsdkijakarta/" class="underline">Instagram</a></li>
-							<li><a href="https://www.youtube.com/c/BPSDKI" class="underline">YouTube</a></li>
-						</ul>
-					</div>
-				</div>
-				<div class="mt-6 text-sm">
-					&copy; 2024 Badan Pusat Statistik Provinsi DKI Jakarta. All rights reserved.
-				</div>
-			</div>
-		</div>
-    </footer>
->>>>>>> 142c80acfcf5d16721a194ce955876762b77e2bd
 </body>
 
 </html>
