55f6945255fd49b5b13a4fcf48f293a823917d8d
Esra Yosefa Simarmata <222212585@stis.ac.id>
2024-12-17 21:55:32 +0700

    sedikit perbaikan footer

--- app/Views/dashboard_admin.php
+++ app/Views/dashboard_admin.php
@@ -1,22 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PST Menjawab | Dashboard</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="<?= base_url('assets/css/styles.css') ?>">
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f9f2f1;
-        }
-    </style>
-</head>
-
-<body class="bg-oranye-1 mt-28 md:mt-16">
-<nav class="bg-white shadow shadow-gray-300 fixed top-0 left-0 w-full px-8 z-50">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>PST Menjawab | Dashboard</title>
+		<script src="https://cdn.tailwindcss.com"></script>
+		<link rel="stylesheet" href="
+					<?= base_url('assets/css/styles.css') ?>">
+		<style>
+			body {
+				font-family: Arial, sans-serif;
+				background-color: #f9f2f1;
+			}
+		</style>
+	</head>
+	<body class="flex flex-col min-h-screen bg-oranye-1 mt-28 md:mt-16 ">
+		<nav class="bg-white shadow shadow-gray-300 fixed top-0 left-0 w-full px-8 z-50">
 			<div class="md:h-16 h-28 mx-auto md:px-4 container flex items-center justify-between flex-wrap md:flex-nowrap">
 				<div class="flex items-center space-x-4">
 					<img src="/assets/images/logo-pst.png" alt="Logo" class="h-10 w-10">
@@ -57,144 +56,147 @@
 				</div>
 			</div>
 		</nav>
-
-    <div class="container mx-auto p-6">
-        <?php if (session()->getFlashdata('message')): ?>
-            <p class="text-green-500"><?= session()->getFlashdata('message') ?></p>
-        <?php endif; ?>
-
-        <!-- Filter Button -->
-        <div class="flex justify-end mb-4">
-            <button id="filterBtn"
-                class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded-full">Filter</button>
-        </div>
-
-        <!-- Filter Modal -->
-        <div id="filterModal"
-            class="fixed inset-0 hidden bg-gray-800 bg-opacity-50 flex items-center justify-center z-50">
-            <div class="bg-white rounded-lg shadow-lg p-6 w-full max-w-md">
-                <h2 class="text-2xl font-bold mb-4">Filter</h2>
-                <form action="/admin/dashboard/filter" method="post" id="filterForm">
-                    <div class="mb-4">
-                        <label for="status" class="block text-sm font-medium text-gray-700">Status</label>
-                        <select id="status" name="status"
-                            class="block w-full p-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-orange-500 focus:outline-none">
-                            <option value="">All</option>
-                            <option value="Sedang diproses">Sedang diproses</option>
-                            <option value="Disetujui">Disetujui</option>
-                            <option value="Ditolak">Ditolak</option>
-                            <option value="Selesai">Selesai</option>
-                        </select>
-                    </div>
-                    <div class="flex justify-end">
-                        <button type="button" id="closeModalBtn"
-                            class="bg-gray-500 text-white py-2 px-4 rounded-full transition duration-300 hover:bg-gray-600 mr-2">Close</button>
-                        <button type="submit"
-                            class="bg-orange-500 text-white py-2 px-4 rounded-full transition duration-300 hover:bg-orange-600">Apply</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-
-        <div class="bg-white shadow-md rounded-lg overflow-hidden">
-            <div class="table-container">
-                <table class="min-w-full bg-white">
-                    <thead class="bg-orange-600 text-white">
-                        <tr>
-                            <th class="py-3 px-5 text-lg">Token</th>
-                            <th class="py-3 px-5 text-lg">Topik</th>
-                            <th class="py-3 px-5 text-lg">Status</th>
-                            <th class="py-3 px-5 text-lg">Aksi</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php if (!empty($requests) && is_array($requests)): ?>
-                            <?php foreach ($requests as $request): ?>
-                                <tr class="border-b">
-                                    <td class="py-2 px-4"><?= esc($request['token_konsultasi']) ?></td>
-                                    <td class="py-2 px-4"><?= esc($request['topik']) ?></td>
-                                    <td class="py-2 px-4"><?= esc($request['status_konsultasi']) ?></td>
-                                    <td class="py-2 px-4">
-                                        <div class="flex justify-around">
-                                            <a href="/admin/consultation/detail/<?= $request['id'] ?>" class="bg-green-500 text-white py-1 px-2 rounded-full w-full text-center mx-1 text-sm transition duration-300 hover:bg-green-600">Detail</a>
-                                            <a href="/admin/consultation/delete/<?= $request['id'] ?>" class="bg-red-500 text-white py-1 px-2 rounded-full w-full text-center mx-1 text-sm transition duration-300 hover:bg-red-600" onclick="return confirm('Apakah Anda yakin ingin menghapus?')">Hapus</a>
-                                        </div>
-                                    </td>
-                                </tr>
-                            <?php endforeach; ?>
-                        <?php else: ?>
-                            <tr>
-                                <td colspan="4" class="py-2 px-4">Tidak ada data.</td>
-                            </tr>
-                        <?php endif; ?>
-                    </tbody>
-                </table>
-            </div>
-
-            <!-- Pagination -->
-            <div class="p-4 bg-white border-t flex justify-center items-center">
-                <div class="flex justify-center items-center space-x-2">
-                    <?php if ($pager): ?>
-                        <?= $pager->links('default', 'tailwind_full') ?>
-                    <?php endif; ?>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="relative" id="footer" >
-			<img src="/assets/images/footer.png" alt="footer" class="w-full">
-			<div class="absolute inset-0 flex flex-col items-center justify-end text-white text-center px-5 text-lg pb-12">
-				<div class="flex justify-between items-center w-full max-w-6xl mb-8 space-x-8">>
-					<div class="w-1/3 text-left">
+		<main class="flex-grow mb-24">
+			<div class="container mx-auto p-6"> <?php if (session()->getFlashdata('message')): ?> <p class="text-green-500"> <?= session()->getFlashdata('message') ?> </p> <?php endif; ?>
+				<!-- Filter Button -->
+				<div class="flex justify-end mb-4">
+					<button id="filterBtn" class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded-full">Filter</button>
+				</div>
+				<!-- Filter Modal -->
+				<div id="filterModal" class="fixed inset-0 hidden bg-gray-800 bg-opacity-50 flex items-center justify-center z-50">
+					<div class="bg-white rounded-lg shadow-lg p-6 w-full max-w-md">
+						<h2 class="text-2xl font-bold mb-4">Filter</h2>
+						<form action="/admin/dashboard/filter" method="post" id="filterForm">
+							<div class="mb-4">
+								<label for="status" class="block text-sm font-medium text-gray-700">Status</label>
+								<select id="status" name="status" class="block w-full p-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-orange-500 focus:outline-none">
+									<option value="">All</option>
+									<option value="Sedang diproses">Sedang diproses</option>
+									<option value="Disetujui">Disetujui</option>
+									<option value="Ditolak">Ditolak</option>
+									<option value="Selesai">Selesai</option>
+								</select>
+							</div>
+							<div class="flex justify-end">
+								<button type="button" id="closeModalBtn" class="bg-gray-500 text-white py-2 px-4 rounded-full transition duration-300 hover:bg-gray-600 mr-2">Close</button>
+								<button type="submit" class="bg-orange-500 text-white py-2 px-4 rounded-full transition duration-300 hover:bg-orange-600">Apply</button>
+							</div>
+						</form>
+					</div>
+				</div>
+				<div class="bg-white shadow-md rounded-lg overflow-hidden">
+					<div class="table-container">
+						<table class="min-w-full bg-white">
+							<thead class="bg-orange-600 text-white">
+								<tr>
+									<th class="py-3 px-5 text-lg">Token</th>
+									<th class="py-3 px-5 text-lg">Topik</th>
+									<th class="py-3 px-5 text-lg">Status</th>
+									<th class="py-3 px-5 text-lg">Aksi</th>
+								</tr>
+							</thead>
+							<tbody> <?php if (!empty($requests) && is_array($requests)): ?> <?php foreach ($requests as $request): ?> <tr class="border-b">
+									<td class="py-2 px-4"> <?= esc($request['token_konsultasi']) ?> </td>
+									<td class="py-2 px-4"> <?= esc($request['topik']) ?> </td>
+									<td class="py-2 px-4"> <?= esc($request['status_konsultasi']) ?> </td>
+									<td class="py-2 px-4">
+										<div class="flex justify-around">
+											<a href="/admin/consultation/detail/
+																<?= $request['id'] ?>" class="bg-green-500 text-white py-1 px-2 rounded-full w-full text-center mx-1 text-sm transition duration-300 hover:bg-green-600">Detail </a>
+											<a href="/admin/consultation/delete/
+																<?= $request['id'] ?>" class="bg-red-500 text-white py-1 px-2 rounded-full w-full text-center mx-1 text-sm transition duration-300 hover:bg-red-600" onclick="return confirm('Apakah Anda yakin ingin menghapus?')">Hapus </a>
+										</div>
+									</td>
+								</tr> <?php endforeach; ?> <?php else: ?> <tr>
+									<td colspan="4" class="py-2 px-4">Tidak ada data.</td>
+								</tr> <?php endif; ?> </tbody>
+						</table>
+					</div>
+					<!-- Pagination -->
+					<div class="p-4 bg-white border-t flex justify-center items-center">
+						<div class="flex justify-center items-center space-x-2"> <?php if ($pager): ?> <?= $pager->links('default', 'tailwind_full') ?> <?php endif; ?> </div>
+					</div>
+				</div>
+			</div>
+		</main>
+        <div class="h-20"></div>
+		<footer class="relative w-full mt-20">
+			<!-- Gambar footer2 di atas kontainer bg-oranye-2 -->
+			<div class="absolute inset-x-0 top-0 -translate-y-full w-full z-0">
+				<img src="/assets/images/footer2.png" alt="footer" class="w-full object-cover">
+			</div>
+			<!-- Kontainer dengan latar belakang oranye -->
+			<div class="relative bg-oranye-2 text-white overflow-hidden pt-20 z-0">
+				<!-- Footer Content -->
+				<div class="container mx-auto px-6 py-12 flex flex-col md:flex-row justify-between space-y-8 md:space-y-0">
+					<!-- Informasi Utama -->
+					<div class="md:w-1/3 flex flex-col space-y-4">
 						<div class="flex items-center space-x-4">
-							<img src="/assets/images/logo-pst.png" alt="Logo" class="h-12 w-12">
-							<h3 class="text-xl font-semibold">Badan Pusat Statistik Provinsi DKI Jakarta</h3>
+							<div>
+								<img src="/assets/images/logo-pst.png" alt="Logo" class="h-12 w-12">
+							</div>
+							<h3 class="text-lg md:text-xl font-semibold leading-tight"> Badan Pusat Statistik Provinsi DKI Jakarta </h3>
 						</div>
-						<p class="mt-4 text-base">
-							Jl. Salemba Tengah No. 36-38 Paseban Senen Jakarta Pusat<br>
-							<span>Phone (021) 31928493</span><br>
-							<span>Fax. (021) 3152004</span><br>
+						<p class="text-sm md:text-base leading-relaxed"> Jl. Salemba Tengah No. 36-38 Paseban Senen Jakarta Pusat <br>
+							<span>Phone: (021) 31928493</span>
+							<br>
+							<span>Fax: (021) 3152004</span>
+							<br>
 							<span>E-mail: bps3100@bps.go.id</span>
 						</p>
 					</div>
-					<div class="w-1/3 text-left">
-						<h4 class="text-xl font-semibold">Website Lainnya:</h4>
-						<ul class="list-none text-base">
-							<li><a href="https://www.bps.go.id" class="underline">Website BPS Indonesia</a></li>
-							<li><a href="https://jakarta.bps.go.id" class="underline">Website BPS Provinsi DKI Jakarta</a></li>
-							<li><a href="https://pst.bps.go.id" class="underline">Website Pelayanan Statistik Terpadu</a></li>
-							<li><a href="https://silastik.bps.go.id" class="underline">Website SILASTIK</a></li>
+					<!-- Website Lainnya -->
+					<div class="md:w-1/3">
+						<h4 class="text-lg md:text-xl font-semibold mb-4">Website Lainnya:</h4>
+						<ul class="space-y-2 text-sm md:text-base">
+							<li>
+								<a href="https://www.bps.go.id" class="underline hover:text-gray-300">Website BPS Indonesia</a>
+							</li>
+							<li>
+								<a href="https://jakarta.bps.go.id" class="underline hover:text-gray-300">Website BPS Provinsi DKI Jakarta</a>
+							</li>
+							<li>
+								<a href="https://pst.bps.go.id" class="underline hover:text-gray-300">Website Pelayanan Statistik Terpadu</a>
+							</li>
+							<li>
+								<a href="https://silastik.bps.go.id" class="underline hover:text-gray-300">Website SILASTIK</a>
+							</li>
 						</ul>
 					</div>
-					<div class="w-1/3 text-left">
-						<h4 class="text-xl font-semibold">Sosial Media:</h4>
-						<ul class="list-none text-base">
-							<li><a href="https://www.facebook.com/bpsdkijakarta/" class="underline">Facebook</a></li>
-							<li><a href="https://x.com/bpsdkijakarta/" class="underline">Twitter</a></li>
-							<li><a href="https://www.instagram.com/bpsdkijakarta/" class="underline">Instagram</a></li>
-							<li><a href="https://www.youtube.com/c/BPSDKI" class="underline">YouTube</a></li>
+					<!-- Sosial Media -->
+					<div class="md:w-1/3">
+						<h4 class="text-lg md:text-xl font-semibold mb-4">Sosial Media:</h4>
+						<ul class="space-y-2 text-sm md:text-base">
+							<li>
+								<a href="https://www.facebook.com/bpsdkijakarta/" class="underline hover:text-gray-300">Facebook</a>
+							</li>
+							<li>
+								<a href="https://x.com/bpsdkijakarta/" class="underline hover:text-gray-300">Twitter</a>
+							</li>
+							<li>
+								<a href="https://www.instagram.com/bpsdkijakarta/" class="underline hover:text-gray-300">Instagram</a>
+							</li>
+							<li>
+								<a href="https://www.youtube.com/c/BPSDKI" class="underline hover:text-gray-300">YouTube</a>
+							</li>
 						</ul>
 					</div>
 				</div>
-				<div class="mt-6 text-sm">
-					&copy; 2024 Badan Pusat Statistik Provinsi DKI Jakarta. All rights reserved.
-				</div>
+				<!-- Copyright -->
+				<div class="relative text-center text-xs md:text-sm mt-4 pb-4"> &copy; 2024 Badan Pusat Statistik Provinsi DKI Jakarta. All rights reserved. </div>
 			</div>
-		</div>
-
-    <script>
-        document.getElementById('dropdownNavbarLink').addEventListener('click', function () {
-            const dropdown = document.getElementById('dropdownNavbar');
-            dropdown.classList.toggle('hidden');
-        });
-        document.getElementById('filterBtn').addEventListener('click', function () {
-            document.getElementById('filterModal').classList.remove('hidden');
-        });
-
-        document.getElementById('closeModalBtn').addEventListener('click', function () {
-            document.getElementById('filterModal').classList.add('hidden');
-        });
-    </script>
-</body>
-
+		</footer>
+		<script>
+			document.getElementById('dropdownNavbarLink').addEventListener('click', function() {
+				const dropdown = document.getElementById('dropdownNavbar');
+				dropdown.classList.toggle('hidden');
+			});
+			document.getElementById('filterBtn').addEventListener('click', function() {
+				document.getElementById('filterModal').classList.remove('hidden');
+			});
+			document.getElementById('closeModalBtn').addEventListener('click', function() {
+				document.getElementById('filterModal').classList.add('hidden');
+			});
+		</script>
+	</body>
 </html>
