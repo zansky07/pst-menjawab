8abfcfc97e80fb48707538637e8698cdf1419ca8
Dwinanda <dwinandam12@gmail.com>
2024-12-20 09:37:55 +0700

    fix feature chatbot

--- app/Views/chatbot.php
+++ app/Views/chatbot.php
@@ -3,7 +3,8 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Chat Page</title>
+	<title>PST Menjawab | Chatbot</title>
+	<link rel="icon" href="/assets/images/logo-pst.png">
 	<script src="https://cdn.tailwindcss.com"></script>
 	<script src="https://unpkg.com/react@18/umd/react.development.js"></script>
 	<script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"></script>
